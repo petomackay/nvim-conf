11db28b02ae0a72a7b475d0b7d150a25db91213c
Peter Mackay <petomackay@gmail.com>
2024-02-11 11:18:57 +0100

    Make the background transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,13 @@
+function SetTransparentBg()
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+end
+
 return {
-    "rose-pine/neovim", name = "rose-pine",
-    priority = 1000,
-    config = function()
-	vim.cmd.colorscheme "rose-pine"
-    end,
+	"rose-pine/neovim",
+	name = "rose-pine",
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme "rose-pine"
+		SetTransparentBg()
+	end,
 }
